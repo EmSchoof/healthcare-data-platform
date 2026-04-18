fc1b42c7d3bada4e625a35eb6827d1798541be98
Emily Schoof <emily.schoof@gmail.com>
2026-04-18 11:22:01 -0700

    separate tasks per branch

--- src/infra/snowflake_setup.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE healthcare;
-CREATE SCHEMA healthcare.raw;
-CREATE SCHEMA healthcare.silver;
-CREATE SCHEMA healthcare.mart;
-
-CREATE OR REPLACE TABLE healthcare.raw.raw_fhir_patient (
-    ingestion_ts TIMESTAMP,
-    payload VARIANT
-);
--- src/libs/dbt/models/sql/patient_metrics.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    state,
-    COUNT(*) AS patient_count,
-    AVG(DATEDIFF(year, birth_date, CURRENT_DATE)) AS avg_age
-FROM {{ ref('stg_patient') }}
-GROUP BY state
