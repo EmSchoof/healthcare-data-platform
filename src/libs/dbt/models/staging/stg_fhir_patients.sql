a6551904871911785d1e8479ba2a2343f3954817
Emily Schoof <emily.schoof@gmail.com>
2026-04-18 18:35:09 -0700

    full end-to-end json-snowflake-dbt workflow w/o errors

--- src/libs/dbt/models/staging/stg_fhir_patients.sql
+++ src/libs/dbt/models/staging/stg_fhir_patients.sql
@@ -1,21 +1,21 @@
 WITH source AS (
     SELECT
-        raw_record
+        raw_payload
     FROM {{ source('raw', 'raw_fhir_patients') }}
 ),
 
 final AS (
     SELECT
-        raw_record:id::string AS patient_id,
-        raw_record:resourceType::string AS resource_type,
-        raw_record:gender::string AS gender,
-        raw_record:birthDate::date AS birth_date,
-        raw_record:name[0]:family::string AS lASt_name,
-        raw_record:name[0]:given[0]::string AS first_name,
-        raw_record:address[0]:city::string AS city,
-        raw_record:address[0]:state::string AS state,
-        raw_record:address[0]:postalCode::string AS postal_code,
-        raw_record:identifier[0]:value::string AS mrn
+        raw_payload:id::string AS patient_id,
+        raw_payload:resourceType::string AS resource_type,
+        raw_payload:gender::string AS gender,
+        raw_payload:birthDate::date AS birth_date,
+        raw_payload:name[0]:family::string AS lASt_name,
+        raw_payload:name[0]:given[0]::string AS first_name,
+        raw_payload:address[0]:city::string AS city,
+        raw_payload:address[0]:state::string AS state,
+        raw_payload:address[0]:postalCode::string AS postal_code,
+        raw_payload:identifier[0]:value::string AS mrn
     FROM source
 )
 SELECT * FROM final
